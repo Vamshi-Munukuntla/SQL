bd63d0e8915f72829ab96e1799d462e70c89152f
Vamshi Munukuntla <vamshi.kumar59@gmail.com>
2022-08-07 16:12:00 +0530

    Add files via upload

---
+++ _02_DDL_Basics.sql
@@ -0,0 +1,11 @@
+drop table if exists employees;
+
+create table employees
+(
+`Employee Name` varchar(100),
+`Employee Id` int ,
+`Manager Name` varchar(100),
+`Division` int,
+PRIMARY KEY (`Employee Id`)
+);
+
